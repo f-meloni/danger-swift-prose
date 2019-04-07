b24b10762bc4f3be7f485544c6faca4656eb9815
Franco Meloni <franco.meloni91@gmail.com>
2019-04-07 20:21:47 +0100

    Version 0.1.0

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/danger/swift.git", from: "1.0.0"),
-        .package(url: "https://github.com/Quick/Nimble", from: "7.3.1"), // dev
-        .package(url: "https://github.com/f-meloni/TestSpy", from: "0.3.1"), // dev
-        .package(url: "https://github.com/orta/Komondor", from: "1.0.2"), // dev
+//        .package(url: "https://github.com/Quick/Nimble", from: "7.3.1"), // dev
+//        .package(url: "https://github.com/f-meloni/TestSpy", from: "0.3.1"), // dev
+//        .package(url: "https://github.com/orta/Komondor", from: "1.0.2"), // dev
         .package(url: "https://github.com/orta/PackageConfig", .exact("0.0.1")),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
-        .package(url: "https://github.com/f-meloni/Rocket", from: "0.8.1"), // dev
+//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
+//        .package(url: "https://github.com/f-meloni/Rocket", from: "0.8.1"), // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
