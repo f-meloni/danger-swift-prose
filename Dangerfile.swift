425a2ed35522736fdc48104e454f0adaa97f2b8e
Franco Meloni <franco.meloni91@gmail.com>
2019-05-08 22:39:41 +0100

    Add Proselint to the Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,4 +2,6 @@ import Danger
 import DangerSwiftProse
 
 let danger = Danger()
+
 MdspellCheck.performSpellCheck(files: ["README.md"], ignoredWords: [], language: "en-us")
+Proselint.performSpellCheck(files: ["README.md"])
