53b2bad6e35ef035991f6d3c7516b724707b751a
Franco Meloni <franco.meloni91@gmail.com>
2020-01-11 13:55:31 +0000

    Move to swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/shibapm/Komondor", from: "1.0.2"), // dev
         .package(url: "https://github.com/shibapm/PackageConfig", from: "0.10.0"), // dev
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
-        .package(url: "https://github.com/f-meloni/Rocket", from: "0.8.1"), // dev
+        .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
